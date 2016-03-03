f13e516ef2ab93e4a0d008e029ca29e770d3f21c
zalelx <alexzhuk163@gmail.com>
2016-03-03 18:21:58 +0400

    ..

--- src/main/java/firstclass.java
+++ src/main/java/firstclass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("fadf");
-        System.out.println();
+        System.out.println("sadglkjsaglk;sadg;lsadf;lksadf");
 
     }
 }
