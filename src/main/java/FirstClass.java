29fb5980904b17541135640936389e5725a1c19c
mickzenkov <zenkovma94@gmail.com>
2016-03-10 09:23:34 +0400

    First commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("fadf");
-        System.out.println("sadglkjsaglk;sadg;lsadf;lksadf");
+        System.out.println("sadglkjsaglk;sadkhjgohdhdgbjhg;lsadf;lksadf");
 
     }
 }
