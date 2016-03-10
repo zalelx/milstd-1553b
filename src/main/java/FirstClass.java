02bc388068089589cc5fe7abb687de8d14a24122
Danny <murzakov.d@gmail.com>
2016-03-10 10:25:13 +0400

    настала новая эра

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,7 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("fadf");
-        System.out.println("sadglkjsaglk;sadkhjgohdhdgbjhg;lsadf;lksadf");
-
+        System.out.println("govno mocha");
     }
 }
