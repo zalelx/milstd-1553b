0704d162355b58565a55ea24fd29a2bc8a596cf9
mickzenkov <zenkovma94@gmail.com>
2016-03-20 16:48:31 +0400

    dfh

--- src/main/java/model/Controller.java
+++ src/main/java/model/Controller.java
@@ -12,11 +12,41 @@ public class Controller {
 
     }
 
-    void ChangeTheLine();
-    int GetAnsWord();
-    void Block();
-    void Unblock();
-    void TestMKO();
+    void ChangeTheLine(){
+        if (Line==TypeOfLine.A) Line=TypeOfLine.B;
+        else Line=TypeOfLine.A;
+        return;
+    }
+
+
+
+    void Block(){
+        Massage.command=NumberOfCommand.BLOCK;
+    }
+
+    void Unblock(){
+
+        Massage.command=NumberOfCommand.UNBLOCK;
+    }
+
+    void TestMKO(){
+        int c=0;
+        for(i=0;i<31;i++){
+            int k=0;
+            if(!SendMassage()){
+                k++;                            //если один раз не прошло
+                if(!SendMassage())              // пробуем еще раз
+                    k++;
+            }
+            // у нас сбой,если k==1 как раз решим, повторно если уйдет
+
+
+            // у нас отказ или генерация
+            if (k==2)
+
+
+
+                int GetAnsWord();
     void FindGenerationObject();
 
 
