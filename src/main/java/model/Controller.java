3266fecc4990e12d2f2543004cf8dae8a873ae23
mickzenkov <zenkovma94@gmail.com>
2016-03-20 17:08:16 +0400

    апоапоа

--- src/main/java/model/Controller.java
+++ src/main/java/model/Controller.java
@@ -48,7 +48,7 @@ public class Controller {
                 c++;
                 j++;
                 if (c==15){
-                    void FindGenerationObject();
+                     FindGenerationObject();
                 }
                 else continue;
 
