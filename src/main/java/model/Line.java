1dd47497fb6b5a96369bbe2fb181f0caa7efce27
zalelx <alexzhuk163@gmail.com>
2016-04-03 14:44:08 +0400

    добавил класс счетчика времени

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -1,11 +1,14 @@
 package model;
 
 import model.message.Message;
+import view.TimeLogger;
+
 import java.util.ArrayList;
 
 public class Line {
     private ArrayList<Port> list = new ArrayList<>();
     private Message message;
+    private boolean hasGeneration = false;
 
 
     public void addPort(Port port) {
@@ -13,8 +16,11 @@ public class Line {
     }
 
     public void broadcastMessage(Message message) {
-        this.message = message;
-        list.forEach(Port::handleMessage);
+        if (!hasGeneration) {
+            this.message = message;
+            TimeLogger.log(message.getTime());
+            list.forEach(Port::handleMessage);
+        }
     }
 
     public Message getMessage() {
