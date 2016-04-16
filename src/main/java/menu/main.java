0b6935b9f648adae3a9a82b598d87b40bacacadc
Danny <murzakov.d@gmail.com>
2016-04-16 19:26:18 +0400

    гуй

---
+++ src/main/java/menu/main.java
@@ -0,0 +1,28 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public Main() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Таблица состояний");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
