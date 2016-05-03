401b0994162ca7c07af15bb3416c768b33095428
zalelx <alexzhuk163@gmail.com>
2016-05-03 22:04:20 +0400

    Добавил задержку времени

--- src/main/java/view/TimeLogger.java
+++ src/main/java/view/TimeLogger.java
@@ -1,18 +1,27 @@
 package view;
 
+import static java.lang.Thread.sleep;
+
 public class TimeLogger {
     private static int currentTime = 0;
 
     public static void log(String string, int time){
-        currentTime += time;
+        delay(time);
+
         log(string);
     }
 
     public static void log(String string) {
+
         System.out.printf("%-30s %s %d\n", string, "Time:", currentTime);
     }
 
     public static void delay(int delay){
+        try {
+            sleep(50 * delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         currentTime += delay;
     }
 }
